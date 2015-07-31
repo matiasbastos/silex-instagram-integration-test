9b1f46d2341548c490bacc3d74d10b8aa23d07e3
Matias Bastos <matias.bastos@gmail.com>
2015-07-31 20:11:46 -0300

    Added config file to store credentials.

--- web/index.php
+++ web/index.php
@@ -1,29 +1,23 @@
 <?php
+use MetzWeb\Instagram\Instagram;
+use Controllers\MediaController;
 
 // to handle statics
 if (array_key_exists('REQUEST_URI', $_SERVER) && preg_match('/\.(?:css|png|jpg|jpeg|gif)$/', $_SERVER['REQUEST_URI'])) {
     return false;
 }
 
-// autoload vendors
 require_once __DIR__.'/../vendor/autoload.php';
 
-// use
-use MetzWeb\Instagram\Instagram;
-use Controllers\MediaController;
-
-// lets create a silex app
 $app = new Silex\Application();
-// debug on
 $app['debug'] = true;
-// dependency injection: twig
 $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../app/Views'));
-// the instagram api object of: https://github.com/cosenary/Instagram-PHP-API
-$app['instagram'] = function () {
+$app->register(new DerAlex\Silex\YamlConfigServiceProvider(__DIR__.'/../config.yml'));
+$app['instagram'] = function () use ($app) {
     return new Instagram(array(
-        'apiKey' => 'b32840dfd1d64dd2becd20ddb86c7e98',
-        'apiSecret' => '164067ddc96544acb66875202fe372a3',
-        'apiCallback' => 'http://localhost:8080/profile',
+        'apiKey' => $app['config']['instagram']['apiKey'],
+        'apiSecret' => $app['config']['instagram']['apiSecret'],
+        'apiCallback' => $app['config']['instagram']['apiCallback'],
     ));
 };
 
