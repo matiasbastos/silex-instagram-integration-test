1c998e7e7c8574651c8d318ea9893be1cad66f0a
Matias Bastos <matias.bastos@gmail.com>
2015-07-31 13:55:26 -0300

    Refactored the code to login and show the user profile of Instagram info to app/Controllers/MediaController.php

--- web/index.php
+++ web/index.php
@@ -17,13 +17,13 @@ $app = new Silex\Application();
 // debug on
 $app['debug'] = true;
 // dependency injection: twig
-$app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/views'));
+$app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../app/Views'));
 // the instagram api object of: https://github.com/cosenary/Instagram-PHP-API
 $app['instagram'] = function () {
     return new Instagram(array(
         'apiKey' => 'b32840dfd1d64dd2becd20ddb86c7e98',
         'apiSecret' => '164067ddc96544acb66875202fe372a3',
-        'apiCallback' => 'http://localhost:8080/redirecturi',
+        'apiCallback' => 'http://localhost:8080/profile',
     ));
 };
 
@@ -31,76 +31,10 @@ $app['instagram'] = function () {
 $app->get('/media/{id}', "Controllers\MediaController::getMediaLocation");
 
 // this url is an instagram login
-$app->get('/', function () use ($app) {
-    $instagram = $app['instagram'];
-    $loginUrl = $instagram->getLoginUrl();
-
-    return $app['twig']->render('login.twig', array('loginUrl' => $loginUrl));
-});
+$app->get('/', "Controllers\MediaController::getInstagramLogin");
 
 // this url shows a gallery with the media of the logged user
-$app->get('/redirecturi', function () use ($app) {
-    $instagram = $app['instagram'];
-    // receive OAuth code parameter
-    $code = $_GET['code'];
-    // check whether the user has granted access
-    if (isset($code)) {
-        // receive OAuth token object
-        $data = $instagram->getOAuthToken($code);
-        $username = $data->user->username;
-        // store user access token
-        $instagram->setAccessToken($data);
-        try {
-            // now you have access to all authenticated user methods
-            $result = $instagram->getUserMedia();
-        } catch (Exception $e) {
-            return $app->redirect('/');
-        }
-    } else {
-        // check whether an error occurred
-        if (isset($_GET['error'])) {
-            return 'An error occurred: '.$_GET['error_description'];
-        }
-    }
-    $mediahtml = '';
-    // display all user likes
-    foreach ($result->data as $media) {
-        $content = '<li>';
-        // output media
-        if ($media->type === 'video') {
-            // video
-            $poster = $media->images->low_resolution->url;
-            $source = $media->videos->standard_resolution->url;
-            $content .= "<video class=\"media video-js vjs-default-skin\" width=\"250\" height=\"250\" 
-                         poster=\"{$poster}\" data-setup='{\"controls\":true, \"preload\": \"auto\"}'>
-                         <source src=\"{$source}\" type=\"video/mp4\" />
-                         </video>";
-        } else {
-            // image
-            $image = $media->images->low_resolution->url;
-            $content .= "<img class=\"media\" src=\"{$image}\"/>";
-        }
-        // create meta section
-        $avatar = $media->user->profile_picture;
-        $username = $media->user->username;
-        $comment = $media->caption->text;
-        $content .= "<div class=\"content\">
-                       <div class=\"avatar\" style=\"background-image: url({$avatar})\"></div>
-                       <p>{$username}</p>
-                       <div class=\"comment\">{$comment}</div>
-                     </div>";
-        // debug media
-        //$mediahtml .= "<xmp>".print_r($media, true)."</xmp>";
-        // output media
-        $mediahtml .= $content.'</li>';
-    }
-    $mediahtml = '<ul class="grid">'.$mediahtml.'</ul>';
-
-    return $app['twig']->render('media.twig', array(
-        'username' => $data->user->username,
-        'media' => $mediahtml,
-    ));
-});
+$app->get('/profile', "Controllers\MediaController::showInstagramMedia");
 
 // run silex
 $app->run();
